91b3cd2c7a82de924248df633e6d0ea64ac643c6
realdevwoong <projung134@gmail.com>
2025-06-21 17:13:21 +0900

    ㄴãString으로 받는 방식

--- src/main/java/com/example/rest_api/controller/ResponseApiController.java
+++ src/main/java/com/example/rest_api/controller/ResponseApiController.java
@@ -1,19 +1,21 @@
 package com.example.rest_api.controller;
 
 import com.example.rest_api.model.UserRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 @RequestMapping("/api/v1")
 public class ResponseApiController {
     @GetMapping("")
-    public UserRequest user(){
+    public String user(){
         var user = new UserRequest();
         user.setUserName("홍길동");
         user.setUserAge(10);
         user.setEmail("hong@gmail.com");
-        return user;
+        log.info("user: {}", user);
+        return user.toString();
     }
 }
