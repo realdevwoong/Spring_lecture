8afb2162187177a9b67a75449975cbdd740aff3f
realdevwoong <projung134@gmail.com>
2025-06-21 17:44:55 +0900

    feat: RequsetMapping, Controller
    ResquestMapping is equal to @GetMapping
    Controller

--- src/main/java/com/example/rest_api/controller/ResponseApiController.java
+++ src/main/java/com/example/rest_api/controller/ResponseApiController.java
@@ -4,14 +4,16 @@ import com.example.rest_api.model.UserRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 @Slf4j
-@RestController
+@Controller
 @RequestMapping("/api/v1")
 public class ResponseApiController {
-    @GetMapping("")
+    @RequestMapping(path="", method= RequestMethod.GET)
     public ResponseEntity<UserRequest> user(){
         var user = new UserRequest();
         user.setUserName("홍길동");
