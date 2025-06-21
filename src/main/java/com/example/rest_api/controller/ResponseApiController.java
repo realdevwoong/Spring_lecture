a29d4a3437360e39302b083a69fbd837044da517
realdevwoong <projung134@gmail.com>
2025-06-21 17:22:46 +0900

    ResponseEntity Status 200 test

--- src/main/java/com/example/rest_api/controller/ResponseApiController.java
+++ src/main/java/com/example/rest_api/controller/ResponseApiController.java
@@ -2,6 +2,8 @@ package com.example.rest_api.controller;
 
 import com.example.rest_api.model.UserRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class ResponseApiController {
     @GetMapping("")
-    public String user(){
+    public ResponseEntity user(){
         var user = new UserRequest();
         user.setUserName("홍길동");
         user.setUserAge(10);
         user.setEmail("hong@gmail.com");
         log.info("user: {}", user);
-        return user.toString();
+        var response = ResponseEntity
+                .status(HttpStatus.OK)
+                .body(user);
+        return response;
     }
 }
