f5c742916b70bb316740569053ba128c99969eb5
realdevwoong <projung134@gmail.com>
2025-06-23 15:21:03 +0900

    serialrization

--- src/main/java/com/example/rest_api/controller/ResponseApiController.java
+++ src/main/java/com/example/rest_api/controller/ResponseApiController.java
@@ -8,11 +8,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/api/v1")
 public class ResponseApiController {
-    @RequestMapping(path="", method= RequestMethod.GET)
-    @ResponseBody
+      @GetMapping("")
+//    @RequestMapping(path="", method= RequestMethod.GET)
+//    @ResponseBody
     public UserRequest user(){
         var user = new UserRequest();
         user.setUserName("홍길동");
--- src/test/java/com/example/rest_api/RestApiApplicationTests.java
+++ src/test/java/com/example/rest_api/RestApiApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.rest_api;
 
+import com.example.rest_api.model.UserRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestApiApplicationTests {
-
+	@Autowired
+	private ObjectMapper objectMapper;
 	@Test
-	void contextLoads() {
+	void contextLoads() throws JsonProcessingException {
+		var user = new UserRequest();
+		user.setUserName("홍길동");
+		user.setUserAge(10);
+		user.setEmail("hong@gmail.com");
+		user.setKorean(true);
+		var json = objectMapper.writeValueAsString(user);
+		System.out.println(json);
 	}
-
 }
